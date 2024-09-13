388ae2b4cdd82115afb27b9af3f7863888518922
mesilov <mesilov.maxim@gmail.com>
2024-09-14 02:06:11 +0600

    Add Local App Authentication storage
    Introduced `LocalAppAuthRepositoryInterface` for handling local app authentication. Implemented `AppAuthFileStorage` for file-based storage of authentication data. Additionally, added `LocalAppAuth` entity to represent authentication information.
    
    Signed-off-by: mesilov <mesilov.maxim@gmail.com>

---
+++ src/Application/Local/Infrastructure/Filesystem/AppAuthFileStorage.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ * This file is part of the bitrix24-php-sdk package.
+ *
+ * © Maksim Mesilov <mesilov.maxim@gmail.com>
+ *
+ * For the full copyright and license information, please view the MIT-LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Bitrix24\SDK\Application\Local\Infrastructure\Filesystem;
+
+use Bitrix24\SDK\Application\Local\Entity\LocalAppAuth;
+use Bitrix24\SDK\Application\Local\Repository\LocalAppAuthRepositoryInterface;
+use Bitrix24\SDK\Core\Exceptions\FileNotFoundException;
+use Bitrix24\SDK\Core\Response\DTO\RenewedAuthToken;
+use JsonException;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Filesystem\Filesystem;
+
+readonly class AppAuthFileStorage implements LocalAppAuthRepositoryInterface
+{
+    public function __construct(
+        private string          $authFileName,
+        private Filesystem      $filesystem,
+        private LoggerInterface $logger
+    )
+    {
+    }
+
+    public function isAvailable(): bool
+    {
+        return $this->filesystem->exists($this->authFileName);
+    }
+
+    /**
+     * @throws FileNotFoundException
+     * @throws JsonException
+     */
+    public function get(): LocalAppAuth
+    {
+        $this->logger->debug('AppAuthFileStorage.get.start', [
+            'authFileName' => $this->authFileName
+        ]);
+
+        if (!$this->filesystem->exists($this->authFileName)) {
+            throw new FileNotFoundException(sprintf('file «%s» with stored access token not found', $this->authFileName));
+        }
+
+        $payload = file_get_contents($this->authFileName);
+        return LocalAppAuth::initFromArray(json_decode($payload['auth_token'], true, 512, JSON_THROW_ON_ERROR));
+    }
+
+    /**
+     * @throws FileNotFoundException
+     * @throws JsonException
+     */
+    public function saveRenewedToken(RenewedAuthToken $renewedAuthToken): void
+    {
+        $this->logger->debug('AppAuthFileStorage.saveRenewedToken.start');
+        $updatedAuth = $this->get();
+        $updatedAuth->updateAuthToken($renewedAuthToken->authToken);
+
+        $this->save($updatedAuth);
+        $this->logger->debug('AppAuthFileStorage.saveRenewedToken.finish');
+    }
+
+    /**
+     * @throws JsonException
+     */
+    public function save(LocalAppAuth $appAuth): void
+    {
+        $this->logger->debug('AppAuthFileStorage.save.start', [
+            'authFileName' => $this->authFileName
+        ]);
+
+        $tokenPayload = json_encode($appAuth->toArray(), JSON_THROW_ON_ERROR);
+        $this->filesystem->dumpFile($this->authFileName, $tokenPayload);
+
+        $this->logger->debug('AppAuthFileStorage.save.finish', [
+            'tokenPayloadSize' => strlen($tokenPayload),
+        ]);
+    }
+}
---
+++ src/Application/Local/Repository/LocalAppAuthRepositoryInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file is part of the bitrix24-php-sdk package.
+ *
+ * © Maksim Mesilov <mesilov.maxim@gmail.com>
+ *
+ * For the full copyright and license information, please view the MIT-LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Bitrix24\SDK\Application\Local\Repository;
+
+use Bitrix24\SDK\Application\Local\Entity\LocalAppAuth;
+use Bitrix24\SDK\Core\Response\DTO\RenewedAuthToken;
+
+interface LocalAppAuthRepositoryInterface
+{
+    public function get(): LocalAppAuth;
+
+    public function saveRenewedToken(RenewedAuthToken $renewedAuthToken): void;
+
+    public function save(LocalAppAuth $appAuth): void;
+
+    public function isAvailable():bool;
+}
