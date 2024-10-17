34a523ed2a9d85fa1765492a8f146c6461a60de8
mesilov <mesilov.maxim@gmail.com>
2024-10-18 01:34:02 +0600

    Add initial setup for local app workflows directory
    Introduced several new files essential for local app workflows. These include configuration and public files, application setup, console commands, and bootstrapping for testing. The added files streamline environment setup, request processing, and logging.
    
    Signed-off-by: mesilov <mesilov.maxim@gmail.com>

---
+++ examples/local-app-workflows/bin/console.php
@@ -0,0 +1,83 @@
+<?php
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+use Examples\Workflows\Application;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\QuestionHelper;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
+use Symfony\Component\Console\SingleCommandApplication;
+
+(new SingleCommandApplication())
+    ->setCode(function (InputInterface $input, OutputInterface $output): int {
+        Application::getLog()->debug('cliCommand.start');
+        try {
+            while (true) {
+                /**
+                 * @var QuestionHelper $helper
+                 *
+                 * method «setCode» override «execute» method for object Command
+                 * we use SingleCommandApplication for reduce code in this example
+                 */
+                // @phpstan-ignore-next-line
+                $helper = $this->getHelper('question');
+                $question = new ChoiceQuestion(
+                    'Please select command',
+                    [
+                        1 => 'workflow template: list',
+                        2 => 'workflow template: add template from file',
+                        3 => 'workflow template: delete added templates',
+                        4 => 'workflow: start',
+                        5 => 'workflow: instances',
+                        6 => 'workflow: terminate',
+                        7 => 'workflow: kill',
+                        8 => 'workflow task: list',
+                        9 => 'workflow task: complete',
+                        0 => 'exit🚪'
+                    ],
+                    null
+                );
+                $question->setErrorMessage('Menu item «%s» is invalid.');
+                $menuItem = $helper->ask($input, $output, $question);
+                $output->writeln(sprintf('You have just selected: %s', $menuItem));
+
+                $sb = Application::getB24Service();
+                switch ($menuItem) {
+                    case 'workflow template: list':
+                        $output->writeln('<info>get workflow template list...</info>');
+
+                        $table = new Table($output);
+                        $table->setHeaders(['ID', 'NAME', 'MODULE_ID', 'ENTITY', 'DOCUMENT_TYPE', 'AUTO_EXECUTE']);
+                        foreach ($sb->getBizProcScope()->template()->list()->getTemplates() as $template) {
+                            $table->addRow(
+                                [
+                                    $template->ID,
+                                    $template->NAME,
+                                    $template->MODULE_ID,
+                                    $template->ENTITY,
+                                    implode(' ', $template->DOCUMENT_TYPE),
+                                    $template->AUTO_EXECUTE->name
+                                ]
+                            );
+                        }
+                        $table->render();
+                        break;
+                    case 'exit🚪':
+                        Application::getLog()->debug('cliCommand.finish');
+                        return Command::SUCCESS;
+                }
+            }
+        } catch (Throwable $exception) {
+            Application::getLog()->debug('cliCommand.error', [
+                'message' => $exception->getMessage(),
+                'trace' => $exception->getTraceAsString()
+            ]);
+            $output->writeln(sprintf('<error>ERROR: %s</error>', $exception->getMessage()));
+            $output->writeln(sprintf('<info>DETAILS: %s</info>', $exception->getTraceAsString()));
+
+            return Command::FAILURE;
+        }
+    })
+    ->run();
---
+++ examples/local-app-workflows/public/install.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * This file is part of the bitrix24-php-sdk package.
+ *
+ * © Maksim Mesilov <mesilov.maxim@gmail.com>
+ *
+ * For the full copyright and license information, please view the MIT-LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+
+use Symfony\Component\HttpFoundation\Request;
+use Examples\Workflows\Application;
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+$incomingRequest = Request::createFromGlobals();
+Application::getLog()->debug('install.init', ['request' => $incomingRequest->request->all(), 'query' => $incomingRequest->query->all()]);
+// try to process ONAPPINSTALL event
+Application::processRequest($incomingRequest)->send();
+?>
+<pre>
+    Application installation started, tokens from Bitrix24:
+    <?= print_r($_REQUEST, true) ?>
+</pre>
+<script src="//api.bitrix24.com/api/v1/"></script>
+<script>
+    BX24.installFinish();
+</script>
