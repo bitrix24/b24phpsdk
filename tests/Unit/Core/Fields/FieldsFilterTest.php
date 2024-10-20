dd13ee9cf019d7cabe6a88a0286e6dba9b92ff84
mesilov <mesilov.maxim@gmail.com>
2024-10-21 01:28:20 +0600

    Add new field filtration methods in FieldsFilter
    Implemented methods to filter user and smart process fields in FieldsFilter. Also fixed filtration for system fields and added corresponding unit tests to ensure functionality.
    
    Signed-off-by: mesilov <mesilov.maxim@gmail.com>

---
+++ tests/Unit/Core/Fields/FieldsFilterTest.php
@@ -0,0 +1,210 @@
+<?php
+
+/**
+ * This file is part of the bitrix24-php-sdk package.
+ *
+ * © Maksim Mesilov <mesilov.maxim@gmail.com>
+ *
+ * For the full copyright and license information, please view the MIT-LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Bitrix24\SDK\Tests\Unit\Core\Fields;
+
+use Bitrix24\SDK\Core\Fields\FieldsFilter;
+use Generator;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(FieldsFilter::class)]
+class FieldsFilterTest extends TestCase
+{
+    #[Test]
+    #[DataProvider('smartProcessFieldsDataProvider')]
+    public function testFilterSmartProcessFields(array $all, array $result): void
+    {
+        $this->assertEquals((new FieldsFilter())->filterSmartProcessFields($all), $result);
+    }
+
+    #[Test]
+    #[DataProvider('systemFieldsDataProvider')]
+    public function testFilterSystemFields(array $all, array $result): void
+    {
+        $this->assertEquals((new FieldsFilter())->filterSystemFields($all), $result);
+    }
+
+    #[Test]
+    #[DataProvider('userFieldsDataProvider')]
+    public function testFilterUserFields(array $all, array $result): void
+    {
+        $this->assertEquals((new FieldsFilter())->filterUserFields($all), $result);
+    }
+
+    public static function smartProcessFieldsDataProvider(): Generator
+    {
+        yield 'empty' => [
+            [
+            ],
+            [
+            ],
+        ];
+        yield 'UF' => [
+            [
+                'UF_CRM_1728045441',
+                'UF_CRM_1728045442'
+            ],
+            [
+            ],
+        ];
+        yield 'system fields' => [
+            [
+                'ID',
+                'NAME'
+            ],
+            [
+            ],
+        ];
+        yield 'system fields + UF' => [
+            [
+                'ID',
+                'NAME',
+                'UF_CRM_1728045441',
+                'UF_CRM_1728045442'
+            ],
+            [
+            ],
+        ];
+        yield 'system fields + UF + smart process' => [
+            [
+                'ID',
+                'NAME',
+                'UF_CRM_1728045441',
+                'UF_CRM_1728045442',
+                'PARENT_ID_1032'
+            ],
+            [
+                'PARENT_ID_1032'
+            ],
+        ];
+        yield 'smart process' => [
+            [
+                'PARENT_ID_1032',
+                'PARENT_ID_1031'
+            ],
+            [
+                'PARENT_ID_1032',
+                'PARENT_ID_1031'
+            ],
+        ];
+    }
+
+    public static function userFieldsDataProvider(): Generator
+    {
+        yield 'empty' => [
+            [
+            ],
+            [
+            ],
+        ];
+        yield 'UF' => [
+            [
+                'UF_CRM_1728045441',
+                'UF_CRM_1728045442'
+            ],
+            [
+                'UF_CRM_1728045441',
+                'UF_CRM_1728045442'
+            ],
+        ];
+        yield 'system fields' => [
+            [
+                'ID',
+                'NAME'
+            ],
+            [
+            ],
+        ];
+        yield 'system fields + UF' => [
+            [
+                'ID',
+                'NAME',
+                'UF_CRM_1728045441',
+                'UF_CRM_1728045442'
+            ],
+            [
+                'UF_CRM_1728045441',
+                'UF_CRM_1728045442'
+            ],
+        ];
+        yield 'system fields + UF + smart process' => [
+            [
+                'ID',
+                'NAME',
+                'UF_CRM_1728045441',
+                'UF_CRM_1728045442',
+                'PARENT_ID_1032'
+            ],
+            [
+                'UF_CRM_1728045441',
+                'UF_CRM_1728045442',
+            ],
+        ];
+    }
+
+    public static function systemFieldsDataProvider(): Generator
+    {
+        yield 'empty' => [
+            [
+            ],
+            [
+            ],
+        ];
+        yield 'UF' => [
+            [
+                'UF_CRM_1728045441',
+                'UF_CRM_1728045442'
+            ],
+            [
+            ],
+        ];
+        yield 'system fields' => [
+            [
+                'ID',
+                'NAME'
+            ],
+            [
+                'ID',
+                'NAME'
+            ],
+        ];
+        yield 'system fields + UF' => [
+            [
+                'ID',
+                'NAME',
+                'UF_CRM_1728045441',
+                'UF_CRM_1728045442'
+            ],
+            [
+                'ID',
+                'NAME'
+            ],
+        ];
+        yield 'system fields + UF + smart process' => [
+            [
+                'ID',
+                'NAME',
+                'UF_CRM_1728045441',
+                'UF_CRM_1728045442',
+                'PARENT_ID_1032'
+            ],
+            [
+                'ID',
+                'NAME'
+            ],
+        ];
+    }
+}
