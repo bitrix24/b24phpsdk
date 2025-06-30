f5d3244127b9e78273a083b54327801d7192d694
Vadim Soluyanov <sallee@info-expert.ru>
2025-06-30 16:48:59 +0400

    add Status class

---
+++ src/Services/CRM/Status/Service/Status.php
@@ -0,0 +1,285 @@
+<?php
+
+/**
+ * This file is part of the bitrix24-php-sdk package.
+ *
+ * © Vadim Soluyanov <vadimsallee@gmail.com>
+ *
+ * For the full copyright and license information, please view the MIT-LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Bitrix24\SDK\Services\CRM\Status\Service;
+
+use Bitrix24\SDK\Attributes\ApiEndpointMetadata;
+use Bitrix24\SDK\Attributes\ApiServiceMetadata;
+use Bitrix24\SDK\Core\Contracts\CoreInterface;
+use Bitrix24\SDK\Core\Credentials\Scope;
+use Bitrix24\SDK\Core\Exceptions\BaseException;
+use Bitrix24\SDK\Core\Exceptions\TransportException;
+use Bitrix24\SDK\Core\Result\AddedItemResult;
+use Bitrix24\SDK\Core\Result\DeletedItemResult;
+use Bitrix24\SDK\Core\Result\FieldsResult;
+use Bitrix24\SDK\Core\Result\UpdatedItemResult;
+use Bitrix24\SDK\Services\AbstractService;
+use Bitrix24\SDK\Services\CRM\Status\Result\StatusResult;
+use Bitrix24\SDK\Services\CRM\Status\Result\StatussResult;
+use Psr\Log\LoggerInterface;
+
+#[ApiServiceMetadata(new Scope(['crm']))]
+class Status extends AbstractService
+{
+    /**
+     * Status constructor.
+     */
+    public function __construct(public Batch $batch, CoreInterface $core, LoggerInterface $logger)
+    {
+        parent::__construct($core, $logger);
+    }
+
+    /**
+     * Creates a new element in the specified reference book
+     *
+     * @link https://apidocs.bitrix24.com/api-reference/crm/status/crm-status-add.html
+     *
+     * @param array{
+     *   ID?: int,
+     *   ENTITY_ID?: string,
+     *   STATUS_ID?: string,
+     *   SORT?: int,
+     *   NAME?: string,
+     *   NAME_INIT?: string,
+     *   SYSTEM?: bool,
+     *   CATEGORY_ID?: int,
+     *   COLOR?: string,
+     *   SEMANTICS?: string,
+     *   EXTRA?: string,
+     * } $fields
+     *
+     * @throws BaseException
+     * @throws TransportException
+     */
+    #[ApiEndpointMetadata(
+        'crm.status.add',
+        'https://apidocs.bitrix24.com/api-reference/crm/status/crm-status-add.html',
+        'Creates a new element in the specified reference book'
+    )]
+    public function add(array $fields): AddedItemResult
+    {
+        return new AddedItemResult(
+            $this->core->call(
+                'crm.status.add',
+                [
+                    'fields' => $fields
+                ]
+            )
+        );
+    }
+
+    /**
+     * Deletes the specified status and all the associated objects.
+     *
+     * @link https://apidocs.bitrix24.com/api-reference/crm/status/crm-status-delete.html
+     *
+     *
+     * @throws BaseException
+     * @throws TransportException
+     */
+    #[ApiEndpointMetadata(
+        'crm.status.delete',
+        'https://apidocs.bitrix24.com/api-reference/crm/status/crm-status-delete.html',
+        'Deletes the specified status and all the associated objects.'
+    )]
+    public function delete(int $id): DeletedItemResult
+    {
+        return new DeletedItemResult(
+            $this->core->call(
+                'crm.status.delete',
+                [
+                    'id' => $id,
+                ]
+            )
+        );
+    }
+
+    /**
+     * Returns the description of the status fields, including user fields.
+     *
+     * @link https://apidocs.bitrix24.com/api-reference/crm/status/crm-status-fields.html
+     *
+     * @throws BaseException
+     * @throws TransportException
+     */
+    #[ApiEndpointMetadata(
+        'crm.status.fields',
+        'https://apidocs.bitrix24.com/api-reference/crm/status/crm-status-fields.html',
+        'Returns the description of the status fields, including user fields.'
+    )]
+    public function fields(): FieldsResult
+    {
+        return new FieldsResult($this->core->call('crm.status.fields'));
+    }
+
+    /**
+     * Returns a status by the status ID.
+     *
+     * @link https://apidocs.bitrix24.com/api-reference/crm/status/crm-status-get.html
+     *
+     *
+     * @throws BaseException
+     * @throws TransportException
+     */
+    #[ApiEndpointMetadata(
+        'crm.status.get',
+        'https://apidocs.bitrix24.com/api-reference/crm/status/crm-status-get.html',
+        'Returns a status by the status ID.'
+    )]
+    public function get(int $id): StatusResult
+    {
+        return new StatusResult($this->core->call('crm.status.get', ['id' => $id]));
+    }
+
+    /**
+     * Get list of status items.
+     *
+     * @link https://apidocs.bitrix24.com/api-reference/crm/status/crm-status-list.html
+     *
+     * @param array   $order     - order of status items
+     * @param array   $filter    - filter array
+     * @param array   $select    = ['ID','ASSIGNED_BY_ID','BEGINDATA','CLIENT_ADDR','CLOSED','CLOSEDATA','COMMENTS','COMPANY_ID','CONTACT_ID','CONTACT_IDS','CONTENT','CREATED_BY_ID','CURRENCY_ID','DATE_CREATE','DATE_MODIFY','DEAL_ID','LEAD_ID','LOCATION_ID','MODIFY_BY_ID','MYCOMPANY_ID','OPENED','OPPORTUNITY','PERSON_TYPE_ID','QUOTE_NUMBER','STATUS_ID','TAX_VALUE','TERMS','TITLE','UTM_CAMPAIGN','UTM_CONTENT','UTM_MEDIUM','UTM_SOURCE','UTM_TERM']
+     * @param integer $startItem - entity number to start from (usually returned in 'next' field of previous 'crm.status.list' API call)
+     *
+     * @throws BaseException
+     * @throws TransportException
+     */
+    #[ApiEndpointMetadata(
+        'crm.status.list',
+        'https://apidocs.bitrix24.com/api-reference/crm/status/crm-status-list.html',
+        'Get list of status items.'
+    )]
+    public function list(array $order, array $filter, array $select, int $startItem = 0): StatussResult
+    {
+        return new StatussResult(
+            $this->core->call(
+                'crm.status.list',
+                [
+                    'order'  => $order,
+                    'filter' => $filter,
+                    'select' => $select,
+                    'start'  => $startItem,
+                ]
+            )
+        );
+    }
+
+    /**
+     * Updates the specified (existing) status.
+     *
+     * @link https://apidocs.bitrix24.com/api-reference/crm/status/crm-status-update.html
+     *
+     * @param array{
+     *   ID?: int,
+     *   ASSIGNED_BY_ID?: int,
+     *   BEGINDATA?: string,
+     *   CLIENT_ADDR?: string,
+     *   CLOSED?: bool,
+     *   CLOSEDATA?: string,
+     *   COMMENTS?: string,
+     *   COMPANY_ID?: int,
+     *   CONTACT_ID?: int,
+     *   CONTACT_IDS?: int[],
+     *   CONTENT?: string,
+     *   CREATED_BY_ID?: int,
+     *   CURRENCY_ID?: string,
+     *   DATE_CREATE?: string,
+     *   DATE_MODIFY?: string,
+     *   DEAL_ID?: int,
+     *   LEAD_ID?: int,
+     *   LOCATION_ID?: int,
+     *   MODIFY_BY_ID?: int,
+     *   MYCOMPANY_ID?: int,
+     *   OPENED?: bool,
+     *   OPPORTUNITY?: string,
+     *   PERSON_TYPE_ID?: int,
+     *   QUOTE_NUMBER?: string,
+     *   STATUS_ID?: string,
+     *   TAX_VALUE?: string,
+     *   TERMS?: string,
+     *   TITLE?: string,
+     *   UTM_CAMPAIGN?: string,
+     *   UTM_CONTENT?: string,
+     *   UTM_MEDIUM?: string,
+     *   UTM_SOURCE?: string,
+     *   UTM_TERM?: string,
+     *   }        $fields
+     *
+     * @throws BaseException
+     * @throws TransportException
+     */
+    #[ApiEndpointMetadata(
+        'crm.status.update',
+        'https://apidocs.bitrix24.com/api-reference/crm/status/crm-status-update.html',
+        'Updates the specified (existing) status.'
+    )]
+    public function update(int $id, array $fields): UpdatedItemResult
+    {
+        return new UpdatedItemResult(
+            $this->core->call(
+                'crm.status.update',
+                [
+                    'id'     => $id,
+                    'fields' => $fields
+                ]
+            )
+        );
+    }
+
+    /**
+     * Count statuss by filter
+     *
+     * @param array{
+     *   ID?: int,
+     *   ASSIGNED_BY_ID?: int,
+     *   BEGINDATA?: string,
+     *   CLIENT_ADDR?: string,
+     *   CLOSED?: bool,
+     *   CLOSEDATA?: string,
+     *   COMMENTS?: string,
+     *   COMPANY_ID?: int,
+     *   CONTACT_ID?: int,
+     *   CONTACT_IDS?: int[],
+     *   CONTENT?: string,
+     *   CREATED_BY_ID?: int,
+     *   CURRENCY_ID?: string,
+     *   DATE_CREATE?: string,
+     *   DATE_MODIFY?: string,
+     *   DEAL_ID?: int,
+     *   LEAD_ID?: int,
+     *   LOCATION_ID?: int,
+     *   MODIFY_BY_ID?: int,
+     *   MYCOMPANY_ID?: int,
+     *   OPENED?: bool,
+     *   OPPORTUNITY?: string,
+     *   PERSON_TYPE_ID?: int,
+     *   QUOTE_NUMBER?: string,
+     *   STATUS_ID?: string,
+     *   TAX_VALUE?: string,
+     *   TERMS?: string,
+     *   TITLE?: string,
+     *   UTM_CAMPAIGN?: string,
+     *   UTM_CONTENT?: string,
+     *   UTM_MEDIUM?: string,
+     *   UTM_SOURCE?: string,
+     *   UTM_TERM?: string,
+     *   } $filter
+     *
+     * @throws \Bitrix24\SDK\Core\Exceptions\BaseException
+     * @throws \Bitrix24\SDK\Core\Exceptions\TransportException
+     */
+    public function countByFilter(array $filter = []): int
+    {
+        return $this->list([], $filter, ['ID'], 1)->getCoreResponse()->getResponseData()->getPagination()->getTotal();
+    }
+}
