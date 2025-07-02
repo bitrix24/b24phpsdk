7705f47ebf2b8fe9bcfee345c1674b0e05da177b
Vadim Soluyanov <sallee@info-expert.ru>
2025-07-02 10:25:16 +0400

    add StatusEntity methods and results

---
+++ src/Services/CRM/Status/Result/StatusEntitiesResult.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This file is part of the bitrix24-php-sdk package.
+ *
+ * © Vadim Soluyanov <vadimsallee@gmail.com>
+ *
+ * For the full copyright and license information, please view the MIT-LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+
+declare(strict_types=1);
+
+namespace Bitrix24\SDK\Services\CRM\Status\Result;
+
+use Bitrix24\SDK\Core\Exceptions\BaseException;
+use Bitrix24\SDK\Core\Result\AbstractResult;
+
+/**
+ * Class StatusEntitiesResult
+ *
+ * @package Bitrix24\SDK\Services\CRM\Status\Result
+ */
+class StatusEntitiesResult extends AbstractResult
+{
+    /**
+     * @return StatusEntityItemResult[]
+     * @throws BaseException
+     */
+    public function getEntities(): array
+    {
+        $res = [];
+        foreach ($this->getCoreResponse()->getResponseData()->getResult()['result'] as $item) {
+            $res[] = new StatusEntityItemResult($item);
+        }
+
+        return $res;
+    }
+}
---
+++ src/Services/CRM/Status/Result/StatusEntityTypesResult.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This file is part of the bitrix24-php-sdk package.
+ *
+ * © Vadim Soluyanov <vadimsallee@gmail.com>
+ *
+ * For the full copyright and license information, please view the MIT-LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+
+declare(strict_types=1);
+
+namespace Bitrix24\SDK\Services\CRM\Status\Result;
+
+use Bitrix24\SDK\Core\Exceptions\BaseException;
+use Bitrix24\SDK\Core\Result\AbstractResult;
+
+/**
+ * Class StatusEntityTypesResult
+ *
+ * @package Bitrix24\SDK\Services\CRM\Status\Result
+ */
+class StatusEntityTypesResult extends AbstractResult
+{
+    /**
+     * @return StatusEntityTypeItemResult[]
+     * @throws BaseException
+     */
+    public function getEntityTypes(): array
+    {
+        $res = [];
+        foreach ($this->getCoreResponse()->getResponseData()->getResult()['result'] as $item) {
+            $res[] = new StatusEntityTypeItemResult($item);
+        }
+
+        return $res;
+    }
+}
