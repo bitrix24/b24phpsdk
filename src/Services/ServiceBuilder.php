72e9a1d733128e85a31b01a1779d2da0abb1cfd4
Sally Fancen <vadimsallee@gmail.com>
2025-08-29 10:57:54 +0400

    fix ServiceBuilder

--- src/Services/ServiceBuilder.php
+++ src/Services/ServiceBuilder.php
@@ -26,7 +26,6 @@ use Bitrix24\SDK\Services\IM\IMServiceBuilder;
 use Bitrix24\SDK\Services\IMOpenLines\IMOpenLinesServiceBuilder;
 use Bitrix24\SDK\Services\Log\LogServiceBuilder;
 use Bitrix24\SDK\Services\Main\MainServiceBuilder;
-use Bitrix24\SDK\Services\Sale\SaleServiceBuilder;
 use Bitrix24\SDK\Services\Telephony\TelephonyServiceBuilder;
 use Bitrix24\SDK\Services\User\UserServiceBuilder;
 use Bitrix24\SDK\Services\UserConsent\UserConsentServiceBuilder;
@@ -210,20 +209,6 @@ class ServiceBuilder extends AbstractServiceBuilder
     {
         if (!isset($this->serviceCache[__METHOD__])) {
             $this->serviceCache[__METHOD__] = new CatalogServiceBuilder(
-                $this->core,
-                $this->batch,
-                $this->bulkItemsReader,
-                $this->log
-            );
-        }
-
-        return $this->serviceCache[__METHOD__];
-    }
-
-    public function getSaleScope(): SaleServiceBuilder
-    {
-        if (!isset($this->serviceCache[__METHOD__])) {
-            $this->serviceCache[__METHOD__] = new SaleServiceBuilder(
                 $this->core,
                 $this->batch,
                 $this->bulkItemsReader,
