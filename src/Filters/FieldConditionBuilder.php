2d44ca07095e7b4ec85ebea2239137706b82c8ed
mesilov <mesilov.maxim@gmail.com>
2026-01-30 01:37:23 +0600

    Mark FieldConditionBuilder as readonly and remove readonly modifiers from constructor arguments
    Signed-off-by: mesilov <mesilov.maxim@gmail.com>

--- src/Filters/FieldConditionBuilder.php
+++ src/Filters/FieldConditionBuilder.php
@@ -22,11 +22,11 @@ use Bitrix24\SDK\Core\Exceptions\InvalidArgumentException;
  *
  * @package Bitrix24\SDK\Filters\Core
  */
-class FieldConditionBuilder
+readonly class FieldConditionBuilder
 {
     public function __construct(
-        private readonly string $fieldName,
-        private readonly AbstractFilterBuilder $filter
+        private string $fieldName,
+        private AbstractFilterBuilder $filter
     ) {
     }
 
