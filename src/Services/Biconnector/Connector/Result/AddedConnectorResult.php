87a829806f61788df1e0c093278bd0705d9be482
Dmitriy Ignatenko <algonexys@gmail.com>
2026-05-13 16:53:45 +0400

    Change author email

--- src/Services/Biconnector/Connector/Result/AddedConnectorResult.php
+++ src/Services/Biconnector/Connector/Result/AddedConnectorResult.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of the bitrix24-php-sdk package.
  *
- * © Dmitriy Ignatenko <titarx@gmail.com>
+ * © Dmitriy Ignatenko <algonexys@gmail.com>
  *
  * For the full copyright and license information, please view the MIT-LICENSE.txt
  * file that was distributed with this source code.
